37dbee45d4e15d46414ca8da8ec7bb0e7fe8145c
Reza Taba <mrtabaa@gmail.com>
2023-07-05 19:35:37 -0700

    show presidents completed

--- client/src/app/components/list-presidents/list-presidents.component.ts
+++ client/src/app/components/list-presidents/list-presidents.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { President } from 'src/app/models/president.model';
 
 @Component({
   selector: 'app-list-presidents',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-presidents.component.scss']
 })
 export class ListPresidentsComponent {
+  presidents: President[] | undefined;
 
+  constructor(private http: HttpClient) { }
+
+  showPresidents(): void {
+    this.http.get<President[]>('http://localhost:5000/api/president/get-all').subscribe(
+      {next: response => this.presidents = response}
+    );
+  }
 }
